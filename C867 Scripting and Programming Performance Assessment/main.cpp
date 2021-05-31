908c54f9d385abb8549e79202033df402ddecd8b
Faith Fernandes <fkfernandes20@gmail.com>
2021-05-31 12:57:22 -0700

    Initial Commit

---
+++ C867 Scripting and Programming Performance Assessment/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  C867 Scripting and Programming Performance Assessment
+//
+//  Created by Faith Fernandes on 5/31/21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
