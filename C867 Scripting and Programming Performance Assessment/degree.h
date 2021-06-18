49d61e7f002e4a9dcde90443b509d15067f400ba
Faith Fernandes <fkfernandes20@gmail.com>
2021-06-17 20:32:22 -0700

    strings mirroring enum DegreeProgram added

--- C867 Scripting and Programming Performance Assessment/degree.h
+++ C867 Scripting and Programming Performance Assessment/degree.h
@@ -1,6 +1,11 @@
 #pragma once
+#include <string>
 using namespace std;
 
+
+
 // C.  Enumerated data type DegreeProgram containing the data type values SECURITY, NETWORK, and SOFTWARE.
-enum DegreeProgram{SECURITY, NETWORK, SOFTWARE};
+// UNDECLARED DegreeProgram added to serve as a default and a unit test; no student should end with an UNDECLARED DegreeProgram
+enum class DegreeProgram{UNDECLARED, SECURITY, NETWORK, SOFTWARE};
+static const string degreeProgramStrings[] =  {"UNDECLARED","SECURITY","NETWORK","SOFTWARE"};
 
