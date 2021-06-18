3e2c76e3c21c356e9445950bf92ba8d6a00b1e5c
Faith Fernandes <fkfernandes20@gmail.com>
2021-06-17 20:44:34 -0700

    added functions to populate student data

--- C867 Scripting and Programming Performance Assessment/student.h
+++ C867 Scripting and Programming Performance Assessment/student.h
@@ -1,46 +1,54 @@
 #pragma once
+#include <iostream>
+#include <iomanip>
 #include "degree.h"
 using namespace std;
 
-// Declarations for student.cpp
+
 
 class Student {
    public:
-   
+      // Size of array for average days to complete a course
+      const static int daysArraySize = 3;
+      
    // D2d. Constructor
+   // Blank Constructor
    Student();
+   // Constructor with parameters
+   Student(string studentID, string studentFirstName, string studentLastName, string studentEmailAddress, int studentAge, int numDays2Complete[], DegreeProgram degreeProgram);
+   
+   // Destructor for Requirement F5
+   ~Student();
    
    // D2a. Accessors
+   string getStudentID();
    string getFirstName();
    string getLastName();
    string getEmailAddress();
    int getAge();
-   vector<int> getNumDays2Complete();
+   const int* getNumDays2Complete();
    DegreeProgram getDegreeProgram();
    
    // D2b. Mutators
-   void setFirstName(string firstName);
-   void setLastName(string lastName);
-   void setEmailAddress(string emailAddress);
-   void setAge(int age);
-   void setNumDays2Complete(int course1Days, int course2Days, int course3Days);
-   void setDegreeProgram(DegreeProgram degPrgm);
+   void setStudentID(string studentID);
+   void setFirstName(string studentFirstName);
+   void setLastName(string studentLastName);
+   void setEmailAddress(string studentEmailAddress);
+   void setAge(int studentAge);
+   void setNumDays2Complete(const int numDays2Complete[]);
+   void setDegreeProgram(DegreeProgram degreeProgram);
    
    // D2e. Print function for specific student data
-   string print();
+   void print();
    
-
-   
-   /*
-      D1.  Create the class Student  in the files student.h and student.cpp, which includes each of the following variables:   student ID, first name, last name, email address, age, array of number of days to complete each course, degree      program
-   */
+   /* D1.  Create the class Student  in the files student.h and student.cpp, which includes each of the following variables:   student ID, first name, last name, email address, age, array of number of days to complete each course, degree program */
    private:
     string studentID;
     string studentFirstName;
     string studentLastName;
     string studentEmailAddress;
     int studentAge;
-    vector<int> numDays2Complete{3};
+    int numDays2Complete[daysArraySize];
     DegreeProgram degreeProgram;
     
 };
